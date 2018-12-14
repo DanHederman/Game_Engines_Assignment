bebfc191f74f992660a2209c0f6707193704ff39
DanHederman <c15410232@mydit.ie>
2018-12-14 22:57:32 +0000

    Controls
    Tunnel now has user controls

--- Game_Engines_Assignment/Assets/Scripts/PhylloGeneratedTunnel.cs
+++ Game_Engines_Assignment/Assets/Scripts/PhylloGeneratedTunnel.cs
@@ -5,11 +5,65 @@ public class PhylloGeneratedTunnel : MonoBehaviour {
     public Transform Tunnel;
     public AudioPeer AudioPeer;
     public float SpeedOfTunnel, DistanceOfCamera;
-	
-	// Update is called once per frame
+
+    private bool control;
+
+    public void Start()
+    {
+        control = false;
+    }
+
+
+
+    // Update is called once per frame
     private void Update () {
 
-        Tunnel.position = new Vector3(Tunnel.position.x, Tunnel.position.y, Tunnel.position.z + (AudioPeer.Amplitude * SpeedOfTunnel));
-        transform.position = new Vector3(transform.position.x, transform.position.y, Tunnel.position.z + DistanceOfCamera);
-	}
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            control = true;
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            control = false;
+        }
+        
+        if(control == true)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                Tunnel.position = new Vector3(Tunnel.position.x, Tunnel.position.y + .5f, Tunnel.position.z + (AudioPeer.Amplitude * SpeedOfTunnel));
+                transform.position = new Vector3(transform.position.x, transform.position.y + .5f, Tunnel.position.z + DistanceOfCamera);
+            }
+
+            else if (Input.GetKey(KeyCode.S))
+            {
+                Tunnel.position = new Vector3(Tunnel.position.x, Tunnel.position.y - .5f, Tunnel.position.z + (AudioPeer.Amplitude * SpeedOfTunnel));
+                transform.position = new Vector3(transform.position.x, transform.position.y - .5f, Tunnel.position.z + DistanceOfCamera);
+            }
+
+            else if (Input.GetKey(KeyCode.A))
+            {
+                Tunnel.position = new Vector3(Tunnel.position.x - .5f, Tunnel.position.y, Tunnel.position.z + (AudioPeer.Amplitude * SpeedOfTunnel));
+                transform.position = new Vector3(transform.position.x - .5f, transform.position.y, Tunnel.position.z + DistanceOfCamera);
+            }
+
+            else if (Input.GetKey(KeyCode.D))
+            {
+                Tunnel.position = new Vector3(Tunnel.position.x + .5f, Tunnel.position.y, Tunnel.position.z + (AudioPeer.Amplitude * SpeedOfTunnel));
+                transform.position = new Vector3(transform.position.x + .5f, transform.position.y, Tunnel.position.z + DistanceOfCamera);
+            }
+
+            else
+            {
+                Tunnel.position = new Vector3(Tunnel.position.x, Tunnel.position.y, Tunnel.position.z + (AudioPeer.Amplitude * SpeedOfTunnel));
+                transform.position = new Vector3(transform.position.x, transform.position.y, Tunnel.position.z + DistanceOfCamera);
+            }
+        }
+        else
+        {
+            Tunnel.position = new Vector3(Tunnel.position.x, Tunnel.position.y, Tunnel.position.z + (AudioPeer.Amplitude * SpeedOfTunnel));
+            transform.position = new Vector3(transform.position.x, transform.position.y, Tunnel.position.z + DistanceOfCamera);
+        }
+    }
 }
